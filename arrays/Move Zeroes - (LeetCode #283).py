cc0ceb6b633934c7f8be564f6598228d572ab8c5
AtharvaZalkikar <atharvazalkikar@gmail.com>
2025-12-04 00:14:47 +0530

    Move Zeroes to End (LeetCode #283) - Swap based approach added.

--- arrays/Move Zeroes - (LeetCode #283).py
+++ arrays/Move Zeroes - (LeetCode #283).py
@@ -24,4 +24,33 @@ for num in nums:
 for j in range(i,len(nums)):
     nums[j]=0
     
-print(nums)
+print(nums) 
+
+
+
+#  APPROACH 2 -  CODE SWAP - MORE DSA STYLE
+
+nums2 = [0, 1, 0, 3, 12]
+
+i = 0
+
+for j in range(len(nums2)):
+    if nums2[j] != 0:
+        nums2[i], nums2[j] = nums2[j], nums2[i]
+        i += 1
+
+print(nums2)
+
+'''
+✅ Swap-Based Two Pointer Method (In-Place & Stable)
+
+Idea
+Use two pointers i and j
+j → scans the array
+i → tracks the position where the next non-zero should go
+
+When you see a non-zero at j, swap it with index i
+Increase i
+
+This method avoids the “fill-zero afterwards” step — it keeps swapping as it goes.
+'''

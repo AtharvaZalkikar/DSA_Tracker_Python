05c65d25fa064eec1bd2526982a35644db792a28
AtharvaZalkikar <atharvazalkikar@gmail.com>
2025-12-05 23:52:43 +0530

    In-Place Merge of Two Sorted Array - (VERY IMPORTANT!) -  This problem appears in interviews at FAANG, Adobe, Microsoft, etc

---
+++ arrays/In-Place Merge of Two Sorted Arrays.py
@@ -0,0 +1,20 @@
+# In-Place Merge of Two Sorted Arrays
+
+'''
+Docstring for arrays.In-Place Merge of Two Sorted Arrays
+
+This problem appears in interviews at FAANG, Adobe, Microsoft, etc.
+
+You are given, for example:
+
+nums1 = [1, 3, 5, 7, _, _, _]   # size = m + n (extra space at end)
+nums2 = [2, 4, 6]               # size = n
+
+Goal:
+Merge nums2 into nums1 so that nums1 becomes:
+[1, 2, 3, 4, 5, 6, 7]
+
+👉 Everything must be done in-place inside nums1.
+'''
+
+# This problem appears in interviews at FAANG, Adobe, Microsoft, etc.

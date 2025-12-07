570301dc815b6bae580fcd8713163125db06378d
AtharvaZalkikar <atharvazalkikar@gmail.com>
2025-12-07 20:19:57 +0530

    Rotate the matrix by 90 degrees Anticlockwise

---
+++ arrays/Rotate Matrix 90 Anti-Clockwise.py
@@ -0,0 +1,53 @@
+# Rotate Matrix 90° Anti-Clockwise
+
+'''
+Docstring for Rotate Matrix 90° Anti-Clockwise
+
+⭐ Key Concept
+
+Clockwise was:
+Transpose
+Reverse each row
+
+Anti-clockwise flips the operations:
+To rotate 90° anti-clockwise:
+👉 Step 1: Transpose the matrix
+(same as before)
+
+👉 Step 2: Reverse each column
+(not rows this time)
+
+That's it.
+'''
+
+matrix = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]
+
+n=len(matrix)
+print(matrix)
+# Step 1 : Transpose matrix
+
+for i in range(n):
+    for j in range(i+1,n):
+        matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+        # print(f"for {i} and {j} the matrix is: {matrix}")                  #for debugging
+
+print(f'Transposed Matrix:{matrix}')
+
+# for column in matrix:
+#     column.reverse()                    <---------> THIS IS WRONG FOR ANTICLOCKWISE AS THIS LOGIC REVERSES ROWS not COLUMNS
+
+# print(f"Rotated Matrix: {matrix}")        
+
+for col in range(n):
+    top = 0
+    bottom = n-1
+    while top<bottom:
+        matrix[top][col],matrix[bottom][col] = matrix[bottom][col],matrix[top][col]
+        top +=1
+        bottom -=1
+
+print(matrix)
